5481d2d1371d78318db8c94a3cca17b217b94f0b
Santinath Mishra <santinath.mishra@intimetec.com>
2021-02-09 20:29:30 +0530

    first commit1

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -14,6 +14,7 @@ namespace WebApplication1
     {
         public static void Main(string[] args)
         {
+            //This is a test
             CreateWebHostBuilder(args).Build().Run();
         }
 
